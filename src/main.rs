575ab38190967b269c04ce31d40d19c3764c6d79
Harsh Agarwal <agarwal.harsh@senrysa.com>
2024-03-07 00:09:01 +0530

    Added clap to parse args and file reader to read the file data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{Parser, Subcommand};
+use std::{ffi::OsString, fs};
+
+#[derive(Parser)]
+#[command(author="Harsh", version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    pattern: String,
+
+    #[arg(short, long)]
+    file_path: OsString, // Update the type here
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Args::parse();
+
+    let file = fs::read_to_string(cli.file_path).expect("Invalid file path or file not found!");
+    println!("File content: {}", file);
 }
